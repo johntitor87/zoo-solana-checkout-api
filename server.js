074e69e6d1660942c86a4002925c978bba63b6c5
johntitor87 <markhendrixks987@gmail.com>
2026-02-25 20:13:40 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-console.log("BOOTING SERVER...");
+console.log("Starting ZOO Solana Checkout API...");
 
-setInterval(() => {
-  console.log("Still alive...");
-}, 5000);
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('ZOO Solana Checkout API is running!');
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on port ${PORT}`);
+});
